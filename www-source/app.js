f40e732113868e7baf46fa11ceae3fc44e932caa
kotcharatp <pear.kc@gmail.com>
2016-03-04 18:11:41 +0700

    Added files via upload

--- www-source/app.js
+++ www-source/app.js
@@ -11,7 +11,6 @@ tl2 = new TimelineMax({ paused:true });
 tl3 = new TimelineMax({ paused:true });
 tl4 = new TimelineMax({ paused:true });
 tl5 = new TimelineMax({ paused:true });
-
 /*
     CUSTOMIZE
 */
@@ -19,78 +18,80 @@ var bottom_margin = 50;
 var top_margin = 50;
 var amp_scale_factor = 20;
 
-
 /*
     DYNAMICALLY CALCULATE PROPER VALUES
 */
 
 // Get the screen size
-var screen_height = window.innerHeight - bottom_margin - top_margin;        // controls
+var screen_height = window.innerHeight;        // controls
 var screen_width = window.innerWidth;
 
 // Find maximum and minimum frequency
-var max_freq = 0;
-var min_freq = 1000000;
 for( var i = 0; i < dataset.length; i++ ) {
-    var o = dataset[i];
-    if( o.f > max_freq ) max_freq = o.f;
-    if( o.f < min_freq ) min_freq = o.f;
+    var o = parseFloat(dataset[i].f);
+    if(i==0) {
+      var max_freq = o;
+      var min_freq = o;
+    }
+
+    if( Number(o) > max_freq ) max_freq = o;
+    if( Number(o) < min_freq ) min_freq = o;
 }
 
 // Find maximum and minimum amplitude
-var max_amp = 0;
-var min_amp = 1000000;
+
 for( var i = 0; i < dataset.length; i++ ) {
-    var o = dataset[i];
-    if( o.a > max_amp ) max_amp = o.a;
-    if( o.a < min_amp ) min_amp = o.a;
+  var o = parseFloat(dataset[i].a);
+    if(i==0) {
+      var max_amp = o;
+      var min_amp = o;
+    }
+    if( o > max_amp ) max_amp = o;
+    if( o < min_amp ) min_amp = o;
 }
 
 // Normalize amplitude by dividing with max_amp
 for( var i = 0; i < dataset.length; i++ ) {
-    dataset[i].a = dataset[i].a / max_amp;
-}
-
-// Calculate step size
-freq_step_size = screen_height /  ( max_freq-min_freq );
+    dataset[i].a = map(dataset[i].a, min_amp, max_amp, 7, 50)
+    dataset[i].f = map(dataset[i].f ,min_freq, max_freq, 50, screen_height-150);
+    }
 
 // calculate duration
 var duration = music_duration / dataset.length;
-
 /*
     ANIMATION
 */
 
-for (var i = 0; i < dataset.length - 40; i++ ) {
+for (var i = 0; i < dataset.length - 15; i++ ) {
     tl1.to(p1, duration, {
-        bottom: (dataset[i].f - min_freq) * freq_step_size,
-        width: dataset[i].a * amp_scale_factor,
-        height: dataset[i].a * amp_scale_factor,
-        borderRadius: (dataset[i].a * amp_scale_factor) / 2
+        bottom: dataset[i].f,
+        width: dataset[i].a ,
+        height: dataset[i].a ,
+        borderRadius: dataset[i].a  / 2
     });
     tl2.to(p2, duration, {
-        bottom: (dataset[i+10].f - min_freq) * freq_step_size,
-        width: dataset[i+10].a * amp_scale_factor,
-        height: dataset[i+10].a * amp_scale_factor,
-        borderRadius: (dataset[i+10].a * amp_scale_factor) / 2
+        bottom: dataset[i+3].f,
+        width: dataset[i+3].a ,
+        height: dataset[i+3].a ,
+        borderRadius: dataset[i+3].a / 2
     });
     tl3.to(p3, duration, {
-        bottom: (dataset[i+20].f - min_freq) * freq_step_size,
-        width: dataset[i+20].a * amp_scale_factor,
-        height: dataset[i+20].a * amp_scale_factor,
-        borderRadius: (dataset[i+20].a * amp_scale_factor) / 2
+        bottom: dataset[i+6].f ,
+        width: dataset[i+6].a ,
+        height: dataset[i+6].a ,
+        borderRadius: dataset[i+6].a / 2
     });
     tl4.to(p4, duration, {
-        bottom: (dataset[i+30].f - min_freq) * freq_step_size,
-        width: dataset[i+30].a * amp_scale_factor,
-        height: dataset[i+30].a * amp_scale_factor,
-        borderRadius: (dataset[i+30].a * amp_scale_factor) / 2
+        bottom: dataset[i+9].f ,
+        width: dataset[i+9].a ,
+        height: dataset[i+9].a ,
+        borderRadius: dataset[i+9].a  / 2
     });
     tl5.to(p5, duration, {
-        bottom: (dataset[i+40].f - min_freq) * freq_step_size,
-        width: dataset[i+40].a * amp_scale_factor,
-        height: dataset[i+40].a * amp_scale_factor,
-        borderRadius: (dataset[i+40].a * amp_scale_factor) / 2
+        bottom: dataset[i+12].f,
+        width: dataset[i+12].a ,
+        height: dataset[i+12].a ,
+        borderRadius: dataset[i+12].a  / 2
     });
 }
 
@@ -101,7 +102,6 @@ tl3.to(p3, 2, { opacity: 0, bottom: 0 });
 tl4.to(p4, 2, { opacity: 0, bottom: 0 });
 tl5.to(p5, 2, { opacity: 0, bottom: 0 });
 
-
 /*
     AUDIO
 */
@@ -109,7 +109,6 @@ tl5.to(p5, 2, { opacity: 0, bottom: 0 });
 var audio = new Howl({
   urls: ['music.wav']
 });
-
 
 /*
     START AND STOP CONTROL
@@ -131,4 +130,9 @@ function stop() {
     tl4.pause();    tl4.seek(0);
     tl5.pause();    tl5.seek(0);
     audio.stop();
+}
+
+function map( x, in_min, in_max, out_min, out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
